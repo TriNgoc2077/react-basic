4966fc67f3d1b8268e7967c0cf0cabf7bf1e7770
Tri Ngoc <caotringoc11@gmail.com>
2025-04-07 23:45:32 +0700

    create todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,18 @@
-import { useState } from "react";
+import "./components/todo/todo.css";
+import TodoNew from "./components/todo/todoNew";
+import TodoData from "./components/todo/todoData";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-	const [count, setCount] = useState(0);
-
+const App = () => {
 	return (
-		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img
-						src={reactLogo}
-						className="logo react"
-						alt="React logo"
-					/>
-				</a>
+		<div className="todo-container">
+			<div className="todo-title">Todo List</div>
+			<TodoNew />
+			<TodoData />
+			<div className="todo-image">
+				<img src={reactLogo} alt="todo" />
 			</div>
-			<h1>Hello World</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
+		</div>
 	);
-}
+};
 
 export default App;
