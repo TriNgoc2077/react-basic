25887359d04c35fbbd9bf451cfff467e9281e7c8
Tri Ngoc <caotringoc11@gmail.com>
2025-04-13 12:18:49 +0700

    upload avatar

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -1,9 +1,10 @@
-import { Drawer } from "antd";
+import { Button, Drawer } from "antd";
 
 const ViewUserDetail = (props) => {
 	const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen } = props;
 	return (
 		<Drawer
+			width={"40vw"} //view width
 			title="Detail User"
 			onClose={() => {
 				setDataDetail(null);
@@ -13,6 +14,41 @@ const ViewUserDetail = (props) => {
 		>
 			{dataDetail ? (
 				<>
+					<div>
+						<img
+							src={`${
+								import.meta.env.VITE_BACKEND_URL
+							}/images/avatar/${dataDetail.avatar}`}
+							alt="avatar"
+							style={{ width: "100px", height: "100px" }}
+						/>
+					</div>
+					<div>
+						<label
+							htmlFor="uploadAvatar"
+							style={{
+								display: "block",
+								width: "fit-content",
+								marginTop: "15px",
+								padding: "5px 10px",
+								background: "orange",
+								borderRadius: "5px",
+								cursor: "pointer",
+								marginBottom: "10px",
+							}}
+						>
+							Chọn Avatar
+						</label>
+						<input
+							type="file"
+							hidden
+							id="uploadAvatar"
+							accept="image/*"
+						/>
+					</div>
+					<Button type="primary">Upload Avatar</Button>
+					<br />
+					<br />
 					<p>ID: {dataDetail._id}</p>
 					<p>Full Name: {dataDetail.fullName}</p>
 					<p>Email: {dataDetail.email}</p>
